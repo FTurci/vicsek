517aafc94b9f6bd1dff4da5093b573ea8c05f054
Francesco Turci <turci@me.com>
2022-02-13 20:50:33 +0000

    Sliders

--- myapp/main.py
+++ myapp/main.py
@@ -3,28 +3,39 @@ import scipy as sp
 from scipy import sparse
 from scipy.spatial import cKDTree
 from bokeh import models, plotting, io
-from bokeh.models import LinearColorMapper
+from bokeh.models import LinearColorMapper, Column, Slider,Div
+from bokeh import models, plotting, io
+from bokeh.layouts import row,column
+import pandas as pd
+from time import sleep
+from itertools import cycle
+import numpy as np
 
 color_mapper = LinearColorMapper(palette="Turbo256", low=-np.pi, high=np.pi)
-#
-L = 32.0
-rho = 4.0
-N = int(rho*L**2)
-print(" N",N)
 
+# side of the box
+L = 32.0
+# initial density
+rho_init = 1.0
+#noise
+eta = 0.15
 r0 = 1.0
 deltat = 1.0
 factor =0.5
 v0 = r0/deltat*factor
-iterations = 10000
-eta = 0.15
-
-
+N = int(rho_init*L**2)
 pos = np.random.uniform(0,L,size=(N,2))
 orient = np.random.uniform(-np.pi, np.pi,size=N)
 
+
+
+
+
+
 def animate():
     global orient, pos
+    N = pos.shape[0]
+
     tree = cKDTree(pos,boxsize=[L,L])
     dist = tree.sparse_distance_matrix(tree, max_distance=r0,output_type='coo_matrix')
 
@@ -46,19 +57,7 @@ def animate():
     pos[pos>L] -= L
     pos[pos<0] += L
 
-    # qv.set_offsets(pos)
-    # qv.set_UVC(cos, sin,orient)
     return pos[:,0], pos[:,1], orient
-#
-# FuncAnimation(fig,animate,np.arange(1, 200),interval=1, blit=True)
-# plt.show()
-
-
-from bokeh import models, plotting, io
-import pandas as pd
-from time import sleep
-from itertools import cycle
-import numpy as np
 
 
 
@@ -71,10 +70,8 @@ data= {'x':x,
 source = models.ColumnDataSource(data)
 
 p = plotting.figure(
-    # x_axis_label="Date",
-    # y_axis_label="New Cases",
+    title="Vicsek model simulator",
     plot_width=600, plot_height=600,
-     # x_axis_type="datetime",
      # tools=["hover", "wheel_zoom"],
      x_range=(0,L),
      y_range=(0,L)
@@ -87,11 +84,38 @@ p.scatter(x="x", y="y",
        line_color={'field': 'angle', 'transform': color_mapper},
        # line_alpha="color",
        size=8,
-       width=4.0
+       width=4.0,
+       
        )
 
-io.curdoc().add_root(p)
-# index_generator = cycle(range(len(california_covid_data.index)))
+
+density_slider = Slider(start=0.1, end=3.5, value=rho_init, step=.1, title="Density")
+noise_slider = Slider(start=0.01, end=1.0, value=0.1, step=.01, title="Noise")
+speed_slider = Slider(start=0.02, end=5.0, value=1.0, step=.02, title="Speed")
+
+def reset(attr, old, new):
+    global orient, pos
+    N = int(density_slider.value*L**2)
+    print(" N",N)
+    pos = np.random.uniform(0,L,size=(N,2))
+    orient = np.random.uniform(-np.pi, np.pi,size=N)
+
+def update_noise(attr,old,new):
+    global eta
+    eta = noise_slider.value
+
+def update_speed(attr,old,new):
+    global v0
+    v0 = speed_slider.value
+
+density_slider.on_change('value', reset)
+noise_slider.on_change('value',update_noise)
+speed_slider.on_change('value',update_speed)
+
+controls = column(density_slider,noise_slider,speed_slider,Div(text='by <a href="https://francescoturci.net" target="_blank">Francesco Turci </a>'))
+
+io.curdoc().add_root(row(p,controls))
+
 
 def stream():
     x,y,angle = animate()
@@ -102,3 +126,4 @@ def stream():
     source.data = data
 
 io.curdoc().add_periodic_callback(stream, 10)
+io.curdoc().title = "Vicsek model simulator"
